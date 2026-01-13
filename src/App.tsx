db34400365db201d991fe2af1987560bf863dd2d
Cecilia Lepik <cecilialepik@gmail.com>
2026-01-13 20:49:05 +0100

    :sparkles: add theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-
-import './App.css'
+import { useState, useReducer, useEffect } from "react";
+import "./App.css";
+import { ThemeContext } from "./context/ThemeContext";
+import { themes, ActionTypeTheme } from "./models/Theme";
+import { SwitchTheme } from "./components/SwitchTheme";
+import { ThemeReducer } from "./reducer/ThemeReducer";
 
 function App() {
+  const root = document.documentElement;
 
+  const [isDarkTheme, setIsDarkTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: light").matches
+  );
+  const [theme, dispatch] = useReducer(ThemeReducer, themes.light);
 
+  useEffect(() => {
+    if (isDarkTheme) {
+      dispatch({ type: ActionTypeTheme.TOGGLED, payload: themes.light });
+    }
+    if (!isDarkTheme) {
+      dispatch({ type: ActionTypeTheme.TOGGLED, payload: themes.dark });
+    }
+    root.style.backgroundColor = theme.backgroundColor;
+  }, [isDarkTheme, theme.backgroundColor, root]);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+
+    return theme.name === themes.light.name
+      ? dispatch({ type: ActionTypeTheme.TOGGLED, payload: themes.dark })
+      : dispatch({ type: ActionTypeTheme.TOGGLED, payload: themes.light });
+  };
   return (
     <>
-     
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <SwitchTheme></SwitchTheme>
+      </ThemeContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
