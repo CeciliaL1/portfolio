2a8d960ac06c6ba21486cafb33965120bf46ca96
Cecilia Lepik <cecilialepik@gmail.com>
2026-01-15 21:02:49 +0100

    :art: add gap to personal-section for improved layout on smaller screens

--- src/components/styled/PersonalSection.tsx
+++ src/components/styled/PersonalSection.tsx
@@ -11,10 +11,21 @@ export const ImageContainerStyle = styled.div`
   height: 250px;
   width: 250px;
   border-radius: 50%;
+
+  @media screen and (max-width: 768px) {
+    height: 200px;
+    width: 200px;
+  }
 `;
 
 export const PersonalSectionStyle = styled.section<IPersonalStyleProps>`
   color: ${(props) => props.color || "#000000"};
   width: 400px;
   letter-spacing: 3px;
+
+  @media screen and (max-width: 768px) {
+    p {
+      margin: 30px;
+    }
+  }
 `;
